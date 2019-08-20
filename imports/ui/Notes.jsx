16b39b13f70486d5497e9844f422d01a0de7a49f
Sagnik <sagnikpaul2882@gmail.com>
2019-08-20 17:37:04 +0530

    Added Notes Page

--- imports/ui/Notes.jsx
+++ imports/ui/Notes.jsx
@@ -6,30 +6,51 @@ export class Notes extends React.Component {
             <div className="notes-container">
                 <div className="sidebar">
                     <div className="details-container">
-                        {/* <img src="" /> */}
-                        <span>sagnikpaul2882@gmail.com</span>
+                        <img className="name-logo" src="/icons-s.svg"/>
+                        <span className="email">sagnikpaul2882@gmail.com</span>
                     </div>
                     <div className="search-container">
                         <input type="text" placeholder="Search all notes.." />
-                        {/* <img src="" /> */}
+                        <img src="/search.png" />
                     </div>
                     <div className="notes-list">
                         <div className="item">
-                            {/* <img src="" /> */}
+                            <img src="/notes.svg" />
                             <p>My First Note</p>
                         </div>
                         <div className="item">
-                            {/* <img src="" /> */}
+                            <img src="/notes.svg" />
                             <p>My Second Note</p>
                         </div>
                         <div className="item">
-                            {/* <img src="" /> */}
+                            <img src="/notes.svg" />
                             <p>My Third Note</p>
                         </div>
                         <div className="item">
-                            {/* <img src="" /> */}
+                        <img src="/notes.svg" />
                             <p>My Fourth Note</p>
                         </div>
+                    </div>
+                </div>
+                <div className="note">
+                    <div className="header">
+                        <p className="title">
+                            My First Notebook
+                        </p>
+                    </div>
+                    <div className="content">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Purus ut faucibus pulvinar elementum integer enim neque volutpat. Donec pretium vulputate sapien nec sagittis aliquam malesuada bibendum arcu. In fermentum posuere urna nec tincidunt praesent. Faucibus nisl tincidunt eget nullam non nisi est. Vel fringilla est ullamcorper eget. Elit ullamcorper dignissim cras tincidunt lobortis. Sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt. Massa sapien faucibus et molestie ac feugiat sed. Lectus sit amet est placerat in egestas erat imperdiet sed. Sit amet risus nullam eget felis eget nunc lobortis. Ac turpis egestas sed tempus urna et. Placerat vestibulum lectus mauris ultrices eros in. Quis varius quam quisque id diam vel. At augue eget arcu dictum varius duis at consectetur lorem. Mauris nunc congue nisi vitae suscipit tellus mauris a diam.
+                            Feugiat nibh sed pulvinar proin gravida hendrerit. Tristique magna sit amet purus. Felis eget nunc lobortis mattis aliquam faucibus purus in. Purus semper eget duis at tellus. Mauris rhoncus aenean vel elit scelerisque. Suspendisse sed nisi lacus sed viverra tellus. Mattis ullamcorper velit sed ullamcorper morbi. Maecenas pharetra convallis posuere morbi leo urna molestie. Odio facilisis mauris sit amet. Sit amet nulla facilisi morbi tempus iaculis urna. Ut enim blandit volutpat maecenas volutpat blandit. Id faucibus nisl tincidunt eget. Amet cursus sit amet dictum sit amet justo donec enim. Gravida quis blandit turpis cursus. Fermentum posuere urna nec tincidunt. A iaculis at erat pellentesque adipiscing commodo elit at imperdiet. Id semper risus in hendrerit gravida rutrum. Lorem donec massa sapien faucibus et molestie ac.
+                            Non arcu risus quis varius. Volutpat blandit aliquam etiam erat velit scelerisque in. Ultrices in iaculis nunc sed augue lacus viverra vitae congue. Iaculis nunc sed augue lacus viverra. Morbi tristique senectus et netus et malesuada fames ac turpis. Egestas fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate sapien. Vulputate mi sit amet mauris commodo. Et magnis dis parturient montes nascetur ridiculus mus mauris. Diam quis enim lobortis scelerisque fermentum. Et ligula ullamcorper malesuada proin libero nunc. Rhoncus aenean vel elit scelerisque. Pulvinar elementum integer enim neque volutpat ac tincidunt vitae semper. Id diam maecenas ultricies mi eget mauris pharetra et ultrices. Ornare arcu odio ut sem. Purus viverra accumsan in nisl nisi scelerisque. Sed euismod nisi porta lorem mollis aliquam. Integer enim neque volutpat ac tincidunt vitae semper. Amet consectetur adipiscing elit duis tristique sollicitudin nibh. Ultrices in iaculis nunc sed augue lacus. Tellus integer feugiat scelerisque varius morbi enim nunc.
+                            Sit amet purus gravida quis blandit. Hac habitasse platea dictumst quisque sagittis purus sit. At urna condimentum mattis pellentesque. Tempus quam pellentesque nec nam aliquam sem et tortor. Mauris sit amet massa vitae tortor condimentum. Odio euismod lacinia at quis risus. At imperdiet dui accumsan sit amet. Sed faucibus turpis in eu mi bibendum. Pellentesque habitant morbi tristique senectus et netus. Tellus cras adipiscing enim eu turpis egestas pretium. Amet consectetur adipiscing elit duis tristique sollicitudin. Viverra nibh cras pulvinar mattis nunc sed blandit. Dui accumsan sit amet nulla facilisi morbi tempus. Cras tincidunt lobortis feugiat vivamus at augue eget arcu. Ac auctor augue mauris augue neque gravida in fermentum.
+                            Id nibh tortor id aliquet lectus proin. Eget gravida cum sociis natoque penatibus. In est ante in nibh mauris cursus. Iaculis at erat pellentesque adipiscing commodo. Auctor urna nunc id cursus metus aliquam. Dignissim diam quis enim lobortis scelerisque fermentum. Urna nunc id cursus metus aliquam eleifend. Sit amet nisl suscipit adipiscing bibendum est ultricies integer quis. At auctor urna nunc id. Consequat semper viverra nam libero justo laoreet sit amet cursus. Pulvinar elementum integer enim neque volutpat ac tincidunt vitae. Sed risus ultricies tristique nulla aliquet enim tortor. Non nisi est sit amet facilisis.
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Purus ut faucibus pulvinar elementum integer enim neque volutpat. Donec pretium vulputate sapien nec sagittis aliquam malesuada bibendum arcu. In fermentum posuere urna nec tincidunt praesent. Faucibus nisl tincidunt eget nullam non nisi est. Vel fringilla est ullamcorper eget. Elit ullamcorper dignissim cras tincidunt lobortis. Sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt. Massa sapien faucibus et molestie ac feugiat sed. Lectus sit amet est placerat in egestas erat imperdiet sed. Sit amet risus nullam eget felis eget nunc lobortis. Ac turpis egestas sed tempus urna et. Placerat vestibulum lectus mauris ultrices eros in. Quis varius quam quisque id diam vel. At augue eget arcu dictum varius duis at consectetur lorem. Mauris nunc congue nisi vitae suscipit tellus mauris a diam.
+                            Feugiat nibh sed pulvinar proin gravida hendrerit. Tristique magna sit amet purus. Felis eget nunc lobortis mattis aliquam faucibus purus in. Purus semper eget duis at tellus. Mauris rhoncus aenean vel elit scelerisque. Suspendisse sed nisi lacus sed viverra tellus. Mattis ullamcorper velit sed ullamcorper morbi. Maecenas pharetra convallis posuere morbi leo urna molestie. Odio facilisis mauris sit amet. Sit amet nulla facilisi morbi tempus iaculis urna. Ut enim blandit volutpat maecenas volutpat blandit. Id faucibus nisl tincidunt eget. Amet cursus sit amet dictum sit amet justo donec enim. Gravida quis blandit turpis cursus. Fermentum posuere urna nec tincidunt. A iaculis at erat pellentesque adipiscing commodo elit at imperdiet. Id semper risus in hendrerit gravida rutrum. Lorem donec massa sapien faucibus et molestie ac.
+                            Non arcu risus quis varius. Volutpat blandit aliquam etiam erat velit scelerisque in. Ultrices in iaculis nunc sed augue lacus viverra vitae congue. Iaculis nunc sed augue lacus viverra. Morbi tristique senectus et netus et malesuada fames ac turpis. Egestas fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate sapien. Vulputate mi sit amet mauris commodo. Et magnis dis parturient montes nascetur ridiculus mus mauris. Diam quis enim lobortis scelerisque fermentum. Et ligula ullamcorper malesuada proin libero nunc. Rhoncus aenean vel elit scelerisque. Pulvinar elementum integer enim neque volutpat ac tincidunt vitae semper. Id diam maecenas ultricies mi eget mauris pharetra et ultrices. Ornare arcu odio ut sem. Purus viverra accumsan in nisl nisi scelerisque. Sed euismod nisi porta lorem mollis aliquam. Integer enim neque volutpat ac tincidunt vitae semper. Amet consectetur adipiscing elit duis tristique sollicitudin nibh. Ultrices in iaculis nunc sed augue lacus. Tellus integer feugiat scelerisque varius morbi enim nunc.
+                            Sit amet purus gravida quis blandit. Hac habitasse platea dictumst quisque sagittis purus sit. At urna condimentum mattis pellentesque. Tempus quam pellentesque nec nam aliquam sem et tortor. Mauris sit amet massa vitae tortor condimentum. Odio euismod lacinia at quis risus. At imperdiet dui accumsan sit amet. Sed faucibus turpis in eu mi bibendum. Pellentesque habitant morbi tristique senectus et netus. Tellus cras adipiscing enim eu turpis egestas pretium. Amet consectetur adipiscing elit duis tristique sollicitudin. Viverra nibh cras pulvinar mattis nunc sed blandit. Dui accumsan sit amet nulla facilisi morbi tempus. Cras tincidunt lobortis feugiat vivamus at augue eget arcu. Ac auctor augue mauris augue neque gravida in fermentum.
+                            Id nibh tortor id aliquet lectus proin. Eget gravida cum sociis natoque penatibus. In est ante in nibh mauris cursus. Iaculis at erat pellentesque adipiscing commodo. Auctor urna nunc id cursus metus aliquam. Dignissim diam quis enim lobortis scelerisque fermentum. Urna nunc id cursus metus aliquam eleifend. Sit amet nisl suscipit adipiscing bibendum est ultricies integer quis. At auctor urna nunc id. Consequat semper viverra nam libero justo laoreet sit amet cursus. Pulvinar elementum integer enim neque volutpat ac tincidunt vitae. Sed risus ultricies tristique nulla aliquet enim tortor. Non nisi est sit amet facilisis.
+                        </p>
                     </div>
                 </div>
             </div>
